5df71dabdaf8ed4eab5f5e5e99f57e2935c36a55
TaekkiMin <tktwin00@gmail.com>
2025-09-09 12:39:38 +0900

    초기 세팅

---
+++ src/main/java/Authorization_Server/AuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package Authorization_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Authorization_Server/AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Authorization_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
